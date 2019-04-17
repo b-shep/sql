4b29bd1c9dea5e49be3cd1c28d02b0f8a5fe50ca
b-shep <b.shep.git@gmail.com>
2019-04-17 09:15:33 -0400

    added more sql files

--- SqlBootcampExample.sql
+++ SqlBootcampExample.sql
@@ -4,12 +4,17 @@ use SqlBootcampExample;
 
 CREATE TABLE Customer (
 	ID int not null primary key auto_increment,
-	Name varchar(50) not null,
-	City varchar(50) not null,
-	State varchar(2) not null,
+	Name nvarchar(50) not null,
+	City nvarchar(50) not null,
+	State nvarchar(2) not null,
 	Sales decimal(18,0) not null,
 	Active bit not null
 );
+
+CREATE TABLE State (
+	Code nvarchar(2) not null primary key,
+    Name nvarchar(50) not null
+    );
 
 Insert Customer (Name, City, State, Sales, Active) Values ('Acme, inc.','Jersey City','NJ',14381891,1);
 Insert Customer (Name, City, State, Sales, Active) Values ('Widget Corp','Seattle','WA',97865829,1);
@@ -21,3 +26,18 @@ Insert Customer (Name, City, State, Sales, Active) Values ('ABC Telecom','Birmin
 Insert Customer (Name, City, State, Sales, Active) Values ('Fake Brothers','Stockton','CA',31226191,1);
 Insert Customer (Name, City, State, Sales, Active) Values ('QWERTY Logistics','Lubbock','TX',17226905,1);
 Insert Customer (Name, City, State, Sales, Active) Values ('Demo, Inc.','Las Vegas','NV',69551557,1);
+Insert into Customer (Name, City, State, Sales, Active)
+	Values
+    ('Super Cool Computers', "Cincy", 'OH', 5674333, 1),
+    ('Target', 'Cincy', 'OH', 538845, 1);
+
+Insert into State (Code, Name)
+	Values
+    ('AL','Alabama'),
+    ('CA','California'),
+    ('HI','Hawaii'),
+    ('NJ','New Jersey'),
+    ('NV','Nevada'),
+    ('OH','Ohio'),
+    ('TX','Texas'),
+    ('WA','Washington');
